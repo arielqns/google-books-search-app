eff643ee0a08e42f9248b30fb107fd47b79fac5b
arielqns <arielqns@gmail.com>
2020-01-03 20:20:51 -0500

    mofidied API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,52 @@
 import axios from "axios";
 
-export default {
-    search: function(query) {
-        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
-    },
-    saveBook: function(bookInfo) {
-        return axios.post(`/${bookInfo.id}`);
+const xhrHeader = {
+  headers: {
+    xhrFields: {
+      withCredentials: true
     }
+  }
+};
+
+export default {
+  // search news using query (hand to server to get newsapi/twinwords results)
+  searchNews: function(search) {
+    console.log("CLIENT API search: " + search);
+    return axios.get("/api/news/" + search)
+  },
+
+  // Gets the article with the given id (for details page)
+  getSavedArticle: function (id) {
+    return axios.get("/api/article/" + id);
+  },
+  
+  // Gets saved articles
+  getSavedArticles: function() {
+    return axios.get("/api/articles");
+  },
+
+  // Deletes the article with the given id
+  deleteArticle: function (id) {
+    return axios.delete("/api/articles/" + id);
+  },
+  
+  // Saves an article to the database
+  saveArticle: function (articleData) {
+    return axios.post("/api/articles", articleData);
+  },
+
+// user functions
+
+  loginUser: function (user) {
+    return axios.post("/api/user/", user, xhrHeader)
+  },
+
+  signup: function (user) {
+    return axios.post("/api/user/signup", user, xhrHeader)
+  },
+
+  authenticateUser: function () {
+    return axios.post("/api/user/authenticate/", xhrHeader)
+  }
+
 };
