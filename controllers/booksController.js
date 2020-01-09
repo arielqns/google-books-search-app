fffed5cac51f9c0d8816664b14629225ca87477b
arielqns <50177867+arielqns@users.noreply.github.com>
2020-01-08 20:00:53 -0500

    Update booksController.js

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -8,10 +8,9 @@ module.exports = {
         .catch(err => res.status(422).json(err));
     },
     create: function(req, res) {
-        console.log("Hitting the create function in controller");
         db.Book
             .create(req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     }
-}
+};
